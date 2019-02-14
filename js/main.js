850a723811744d7c2594e2b7574fcaec7ffa21de
Norah C. IV <Duynv.nde16013@vtc.edu.vn>
2019-02-15 02:21:12 +0700

    Oke

--- js/main.js
+++ js/main.js
@@ -4,18 +4,7 @@
   
   //Begin - Window Load
   $(window).load(function(){
-  
-  
-      //==============___Page Loader___================
-    
-    $('#page-loader').delay(300).fadeOut(400, function(){
-  
-    });
-  
-    $('#loader-name').addClass('loader-left');
-    $('#loader-job').addClass('loader-right');
-    $('#loader-animation').addClass('loader-hide');
-  
+    $('spinner-loading').delay(2000).fadeOut(4000, function(){});
   });
   
   //Begin - Document Ready
